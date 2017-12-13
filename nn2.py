1516cb95c2d1195f2110f3ee850356513f85dc01
iphyer <mshen32@wisc.edu>
2017-12-12 19:50:48 -0600

    NN

--- nn2.py
+++ nn2.py
@@ -2,37 +2,18 @@
 """
 Created on Sun Dec 10 16:36:55 2017
 
-@author: sheen
+@author: Myron
 """
-
-
-# In[1]:
-
-
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# we'll want this for plotting
-import matplotlib.pyplot as plt
-
-# we'll want this for text manipulation
-import re
-
-# for quick and dirty counting
-from collections import defaultdict
-
-# the Naive Bayes model
-from sklearn.naive_bayes import MultinomialNB
+# data processing, CSV file I/O (e.g. pd.read_csv)
+import pandas as pd 
 # function to split the data for cross-validation
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import ShuffleSplit
-# function for cross validation
-from sklearn.model_selection import cross_val_score
-# function for transforming documents into counts
-from sklearn.feature_extraction.text import CountVectorizer
+
+from sklearn.preprocessing import StandardScaler
+
 from sklearn.neural_network import MLPClassifier
-# function for encoding categories
-from sklearn.preprocessing import LabelEncoder
+
+from sklearn.metrics import classification_report,confusion_matrix
 
 
 # In[3]:
@@ -46,55 +27,63 @@ from sklearn.preprocessing import LabelEncoder
 # In[4]:
 
 
-twigen = pd.read_csv("louis_users_all_features_label_1207_updated.csv", encoding='ANSI')
-# names = ["user_key", "user_id", "user_account", "user_name", "user_location", "user_profile", "user_start_time", "user_fname", "user_lname", "user_mname", "gender", "user_follower", "user_tweet_count", "freqWeekDay"]
+data_all = pd.read_csv("./data/louis_users_all_features_label_1207_updated.csv", encoding='ISO-8859-1')
 
-twigen.head()
+instance_data = data_all[ ['freqWeekDay' ,'freqWeekend' ,'freqMoring' ,'freqNoon'
+      ,'freqAfternoon' ,'freqNight' ,'freqLateNight' ,'freqJan' ,'freqFeb' ,'freqMar' 
+      ,'freqApr' ,'freqMay','freqJun' ,'freqJul' ,'freqAug' ,'freqSep' ,'freqOct' 
+      ,'freqNov' ,'freqDec' ,'frequency_1','frequency_2' ,'frequency_3' ,'frequency_4' 
+      ,'frequency_5' ,'frequency_6' ,'tralength_1_2' ,'tralength_1_3' ,'tralength_1_4'
+      ,'tralength_1_5' ,'tralength_1_6' ,'tralength_2_3' ,'tralength_2_4' ,'tralength_2_5'
+      ,'tralength_2_6' ,'tralength_3_4','tralength_3_5' ,'tralength_3_6' ,
+      'tralength_4_5' ,'tralength_4_6' ,'tralength_5_6'] ]
 
+# deal with NA
 
-# In[5]:
+instance_data = instance_data.fillna(0)
 
-
-twigen['gender'].head()
-#twigen['description_norm'] = [normalize_text(s) for s in twigen['description']]
-twigen2 = [twigen['freqWeekDay'], twigen['freqWeekend'], twigen['freqMoring'], twigen['freqNoon'], 
-     twigen['freqAfternoon'], twigen['freqNight'], twigen['freqLateNight'], twigen['freqJan'], twigen['freqFeb'], twigen['freqMar'], twigen['freqApr'], twigen['freqMay'], 
-     twigen['freqJun'], twigen['freqJul'], twigen['freqAug'], twigen['freqSep'], twigen['freqOct'], twigen['freqNov'], twigen['freqDec'], twigen['frequency_1'], 
-     twigen['frequency_2'], twigen['frequency_3'], twigen['frequency_4'], twigen['frequency_5'], twigen['frequency_6'], twigen['tralength_1_2'], twigen['tralength_1_3'], twigen['tralength_1_4'], 
-     twigen['tralength_1_5'], twigen['tralength_1_6'], twigen['tralength_2_3'], twigen['tralength_2_4'], twigen['tralength_2_5'], twigen['tralength_2_6'], twigen['tralength_3_4'],
-     twigen['tralength_3_5'], twigen['tralength_3_6'], twigen['tralength_4_5'], twigen['tralength_4_6'], twigen['tralength_5_6']]
-df = pd.DataFrame(twigen2)
-df = df.T
-X = list()
-
-for index, row in df.iterrows():
-# for x in df[:]:
-    X.append(row.tolist())
-    
-encoder = LabelEncoder()
-#print(twigen['gender'])
-twigen['gender']=twigen['gender'].fillna("")
-y = encoder.fit_transform(twigen['gender'])    
-
-# In[6]:
-
-# pull the data into vectors
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X = instance_data
+y = data_all['gender']
 
 
-# In[7]:
-# =============================================================================
+# split training and testing dateset
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-clf = MLPClassifier(solver='sgd', alpha=0.0001, hidden_layer_sizes=(5, 2), random_state=1)
+scaler = StandardScaler()
+# Fit only to the training data
+scaler.fit(X_train)
 
-clf.fit(x_train, y_train)                         
-MLPClassifier(activation='logistic', alpha=1e-05, batch_size=10,
-       beta_1=0.9, beta_2=0.999, early_stopping=False,
-       epsilon=1e-08, hidden_layer_sizes=(5, 2), learning_rate='constant',
-       learning_rate_init=0.001, max_iter=200, momentum=0.9,
-       nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
-       solver='sgd', tol=0.0001, validation_fraction=0.1, verbose=False,
-       warm_start=False)
+# Now apply the transformations to the data:
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
-clf.predict(x_train)
+mlp = MLPClassifier(activation='relu', alpha=1e-2,batch_size=5,hidden_layer_sizes=(100,100,100,100,100))
+mlp.fit(X_train,y_train)
+
+predictions = mlp.predict(X_test)
+
+print(confusion_matrix(y_test,predictions))
+print(classification_report(y_test,predictions))
+#
+#for index, row in df.iterrows():
+## for x in df[:]:
+#    X.append(row.tolist())
+
+
+
+
+
+
+#clf = MLPClassifier(solver='sgd', alpha=0.0001, hidden_layer_sizes=(5, 2), random_state=1)
+
+#clf.fit(x_train, y_train)                         
+#MLPClassifier(activation='logistic', alpha=1e-05, batch_size=10,
+#       beta_1=0.9, beta_2=0.999, early_stopping=False,
+#       epsilon=1e-08, hidden_layer_sizes=(5, 2), learning_rate='constant',
+#       learning_rate_init=0.001, max_iter=200, momentum=0.9,
+#       nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
+#       solver='sgd', tol=0.0001, validation_fraction=0.1, verbose=False,
+#       warm_start=False)
+
+#clf.predict(x_train)
 
