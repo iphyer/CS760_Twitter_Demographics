2363dcb668ae38c8873354315970979185568f53
XinyiHolly <xliuholly@gmail.com>
2017-12-16 22:44:48 -0600

    newN

--- Name.py
+++ Name.py
@@ -8,7 +8,8 @@ Created on Sat Dec 16 21:19:03 2017
 # data processing, CSV file I/O (e.g. pd.read_csv)
 import pandas as pd 
 
-# 
+# time
+import time 
 # !! Must have gender.py in the file
 
 from gender import getGenders
@@ -31,12 +32,14 @@ for i in namelist:
 # Groupby user_id then apply the needed functions
 for row_index,row in data_all.iterrows():
     usr_fname = row["user_fname"]
+    time.sleep(1)
     #result = getGenders(usr_fname)
     out = getGenders(usr_fname)
     if len(out[0]) != 3:
         print(row_index)
         print(out)
     else:
+        print(out)
         if (out[0][0] == 'male'):
             data_all[row_index,"user_predicted_fname_genderM"] = out[0][1]
             data_all[row_index,"user_fname_count"] = out[0][2]
